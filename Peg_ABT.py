584bde919815ace6ab263b3156eb2782aba50782
blake <blake.hannaford@gmail.com>
2018-05-04 11:00:27 -0700

    adding leaf names

--- Peg_ABT.py
+++ Peg_ABT.py
@@ -19,43 +19,6 @@ demo_bt = b3.BehaviorTree()
 LeafDebug = False
 SolverDebug = False
 
-
-# some examples from IKBT
-
-#tanSolver = tan_solve()
-#tanSolver.BHdebug = SolverDebug
-#tanSolver.Name = "Tangent Solver"
-
-#tanSol = b3.Sequence([tanID, tanSolver])
-#tanSol.Name = "TanID+Solv"
-#tanSol.BHdebug =  LeafDebug
-
-
-#algID = algebra_id()
-#algID.Name = "Algebra ID"
-#algID.BHdebug = LeafDebug
-
-#algSolver = algebra_solve()
-#algSolver.Name = "Algebra Solver"
-##algSolver.BHdebug = LeafDebug
-
-#algSol = b3.Sequence([algID, algSolver])
-#algSol.Name = "Algebra ID and Solve"
-#algSol.BHdebug = SolverDebug
- 
-#           ONE BT TO RULE THEM ALL!
-#   Higher level BT nodes here
-##
-
-#sc_tan = b3.Sequence([b3.OrNode([tanSol, scSol]), rk])
-#worktools = b3.Priority([algSol, sc_tan, Simu_Eqn_Sol, sacSol, x2z2_Solver])
-#subtree = b3.RepeatUntilSuccess(b3.Sequence([asgn, worktools]), 6)
-#solveRoutine = b3.Sequence([sub_trans, subtree,  updateL, compDetect])
-
-#topnode = b3.RepeatUntilSuccess(solveRoutine, 7) #max 10 loops
-
-#ikbt.root = topnode 
-
 NSYMBOLS = 30 # number of VQ symbols for observations
 
 T = True
@@ -75,7 +38,7 @@ def gaussian(x, mu, sig):
 class aug_leaf(b3.Action): 
     def __init__(self,probSuccess):
         b3.BaseNode.__init__(self)
-        # Transition Probabilties for this leaf
+        # Transition Probabilities for this leaf
         self.pS = probSuccess
         self.pF = 1.0-self.pS
         #  Observation Densities for this leaf
@@ -96,13 +59,16 @@ class aug_leaf(b3.Action):
             b += self.Obs[j]
             #print "Obs: b,a", b,a
             if b >= a:
-                return j
+                return j;
         return j
     
     def tick(self,tick):
+        f = tick.blackboard.get('logfileptr')
         a = random.uniform(0,1.0)
         #print "random: ",a , "   Ps: ", self.pS
-        print "------------------------------------> obs: ", self.gen_obs()
+        observation = self.gen_obs()
+        print "------------------------------------> obs: ", observation
+        f.write(self.Name+', '+str(observation)+'\n')
         if a<self.pS:
             return b3.SUCCESS 
         else:
@@ -113,16 +79,18 @@ print"\n\n"
     
 leaf1 = aug_leaf(1.0) 
 leaf1.set_Obs_Density(5,1)
+leaf1.Name = 'L1'
 
 
 ########  Step 2 Insert and Grasp block
     
 leaf2a = aug_leaf(0.9)
 leaf2a.set_Obs_Density(12,1)
+leaf2a.Name = 'L2a'
 
 leaf2c = aug_leaf(0.95)
 leaf2c.set_Obs_Density(12,1)
-
+leaf2c.Name = 'L2c'
 
 leaf2b = b3.Sequence([leaf2a,leaf2c])
 leaf2 = b3.RepeatUntilSuccess(leaf2b,2) 
@@ -132,36 +100,44 @@ leaf2 = b3.RepeatUntilSuccess(leaf2b,2)
 
 leaf3to5 = aug_leaf(1.0)
 leaf3to5.set_Obs_Density(5,1)
+leaf3to5.Name = 'L345'
 
 ##########  Step 6 Insert Right grasper / grasp
 
 leaf6a = aug_leaf(0.6)
 leaf6a.set_Obs_Density(18,1)
+leaf6a.Name = 'L6a'
 
 leaf6c = aug_leaf(0.75)
 leaf6c.set_Obs_Density(18,1)
+leaf6c.Name = 'L6c'
 
 leaf6b = b3.Sequence([leaf6a,leaf6c])
 leaf6 = b3.RepeatUntilSuccess(leaf6b,2)
-
 leaf6.Name = "Leaf 6"
 
 ########  Steps 7-9   Release Left / Reorient / Position
 
 leaf7to9 = aug_leaf(1.0)
 leaf7to9.set_Obs_Density(5,1)
+leaf7to9.Name = 'L789'
 
 ########  Step 10     Place on peg / Release / Clear 
      
 leaf10a = aug_leaf(0.9)
 leaf10a.set_Obs_Density(22,1)
+leaf10a.Name = 'L10a'
 
 leaf10c = aug_leaf(0.95)
 leaf10c.set_Obs_Density(22,1)
+leaf10c.Name = 'L10c'
     
 leaf10d = aug_leaf(0.8)
 leaf10d.set_Obs_Density(22,1)
-leaf10b = b3.Sequence([leaf10a,leaf10c])
+leaf10d.Name = 'L10d'
+
+leaf10b = b3.Sequence([leaf10a,leaf10c,leaf10d])
+
 leaf10 = b3.RepeatUntilSuccess(leaf10b,2) 
 
 ###    Debugging
@@ -172,6 +148,10 @@ N1 = b3.Sequence([leaf1, leaf2, leaf3to5, leaf6, leaf7to9, leaf10])
 N1.Name = 'Sequencer Node'
 N1.BHdebug = T
 bb = b3.Blackboard()
+
+# open the log file
+logf = open(logdir+'statelog.txt','w')
+bb.set('logfileptr',logf)
 
 demo_bt.root = N1
 
