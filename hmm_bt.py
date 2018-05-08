fed88c45bb0275619efb9c5e35bff5ee976edc80
blake <blake.hannaford@gmail.com>
2018-05-08 16:46:57 -0700

    HMM identification starting to work?

--- hmm_bt.py
+++ hmm_bt.py
@@ -7,6 +7,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#sudo pip install scikit-learn  # dep for hmmlearn
+#pip install -U --user hmmlearn 
 from hmmlearn import hmm
 
 # INITIAL State Transition Probabilities
@@ -22,7 +24,7 @@ A[4,16] = 0.10
 A[5,6] = 0.95
 A[5,16] = 0.05
 A[6,7] = 1
-A[7,8] = 0.090
+A[7,8] = 0.90
 A[7,10] = 0.10
 A[8,9] = 0.05
 A[8,11] = 0.95
@@ -37,16 +39,106 @@ A[13,14] = 0.95
 A[13,16] = 0.05
 A[14,15] = 0.80
 A[14,16] = 0.20
+A[15,15] = 1.0
+A[16,16] = 1.0
 
 A = A[1:17,1:17]  # get zero offset index
 
-names = ['l1','l2a1','l2b1','l2a2','l2b2', 'l345', 'l6a1', 'l6b1', 'l6a2', ';6b2', 'l789', 'l10a1', 'l10b1', 'l10c1']
 
+print "Original  A matrix:"
+for i in range(16):
+    for j in range(16):
+        print '{:.3f} '.format(A[i,j]),
+    print '\n' 
+    
+print "A-matrix row check"
+for i in range(16):
+    r = 0
+    for j in range(16):
+        r += A[i,j]
+    print i, r
+    
+
+names = ['l1','l2a1','l2b1','l2a2','l2b2', 'l345', 'l6a1', 'l6b1', 'l6a2', ';6b2', 'l789', 'l10a1', 'l10b1', 'l10c1', 'OutS', 'OutF']
+
+sig = 2.0
+outputs = {'l1':2, 'l2a1': 4, 'l2b1':6, 'l2a2':8 ,'l2b2':10, 'l345':12, 'l6a1':14, 'l6b1':16, 'l6a2':18, ';6b2':20, 'l789':22, 'l10a1':24, 'l10b1':26, 'l10c1':28, 'OutS':30, 'OutF':30}
 #
 Pi = np.zeros(16)
 Pi[0] = 1.0      # always start at state 1
 
 
-M = hmm.GaussianHMM(n_components=16, covariance_type='diag', n_iter=10)
-M.n_features = 1
+M = hmm.GaussianHMM(n_components=16, covariance_type='diag', n_iter=10,init_params='')
+#M.n_features = 1
+M.startprob_ = Pi
+M.transmat_ = A
+tmpmeans = []
+for i in range(len(names)):
+    tmpmeans.append( [ outputs[names[i]] ] )
+M.means_ = np.array(tmpmeans)
+tmpcovars = sig* np.ones((16))
+tmpcovars.shape = [16,1]
+M.covars_ = np.array(tmpcovars)
 
+
+# read in data file
+
+
+
+logdir = 'logs/'
+logf = open(logdir+'statelog.txt','r')
+
+X = []   # state names 
+Y = []   # observations
+Ls =[]   # lengths 
+
+seq = [] # current state seq
+os  = [] # current obs seq
+
+for line in logf:
+   #print '>>>',line
+   line = line.strip()
+   if line == '---': 
+       Ls.append(len(os)) 
+       os  = []
+   else:
+       [state, obs ] = line.split(',')
+       X.append(state)
+       Y.append([int(obs)])
+       os.append([int(obs)])
+
+
+print "starting HMM fit with ", len(Y), ' sequences.'   
+ 
+quit() 
+ 
+Y=np.array(Y).reshape(-1,1)  # make 2D
+Ls = np.array(Ls)
+M.fit(Y,Ls)
+
+np.set_printoptions(precision=3,suppress=True)
+
+print "New A matrix:"
+for i in range(len(names)):
+    for j in range(len(names)):
+        print '{:.3f} '.format(M.transmat_[i,j]),
+    print '\n' 
+
+quit()
+
+#print "shapes:"
+#print "outputs", len(outputs)
+#print "means_", (M.means_.shape)
+#print "covars", (tmpcovars.shape)
+#print "trans",  (M.transmat_.shape)
+
+
+# Generate sample data
+#X, Z = M.sample(50)
+#print X.shape, Z.shape 
+#print type(X), type(Z)
+
+#for i in range(len(Z)):
+    #print names[Z[i]], int(0.5 + X[i,0])
+    
+    
--- peg2.py
+++ peg2.py
@@ -23,9 +23,9 @@ demo_bt = b3.BehaviorTree()
 LeafDebug = False
 SolverDebug = False
 
-NSYMBOLS = 30 # number of VQ symbols for observations
+NSYMBOLS = 150 # number of VQ symbols for observations
 
-NEpochs = 10  # number of simulations
+NEpochs = 1000  # number of simulations
 
 T = True
 F = False
@@ -52,12 +52,13 @@ class aug_leaf(b3.Action):
         
     def set_Obs_Density(self, mu, sig):
         if (mu+sig) > NSYMBOLS or ((mu-sig) < 0):
-            print "Bad observation init"
-            quit()
+            print 'aug_leaf: Warning may gen negative observations'
+            #quit()
         for j in range(NSYMBOLS):
             self.Obs[j] = gaussian(j+0.5,mu,sig)
             #print "j/Obs:",j,self.Obs[j]
- 
+        print self.Name, 'obs:', mu, sig
+        
     def gen_obs(self):
         a = random.uniform(0,1.0)
         b = 0.0
@@ -82,36 +83,48 @@ class aug_leaf(b3.Action):
 print"\n\n"
 
 
-sig = 2.0   # observation sigma
+sig = 2.0
+outputs = {'l1':2, 'l2a1': 5, 'l2b1':8, 'l2a2': 8,  'l2b2':11, 'l345':14, 'l6a1':17, 'l6b1':20, 'l6a2':23, 'l6b2':26, 'l789':29, 'l10a1':33, 'l10b1':36, 'l10c1':28, 'OutS':30, 'OutF':30}
+
+##  Regenerate output means:
+i = 0
+di = 8
+for n in outputs.keys():
+    outputs[n] = i
+    i += di
+    
+    
+#print outputs
+#quit()
+ 
 
 ########  Step 1  Position Left Grasper over block
     
 l1 = aug_leaf(1.0) 
-l1.set_Obs_Density(2,sig)
-l1.Name = 'L1'
-
+l1.set_Obs_Density(outputs['l1'], sig)
+l1.Name = 'l1'
 
 ########  Step 2 Insert and Grasp block
 
 # try 1    
 l2a1 = aug_leaf(0.9)
-l2a1.set_Obs_Density(4,sig)     # emit "2"
-l2a1.Name = 'L2a1'
+l2a1.Name = 'l2a1'
+l2a1.set_Obs_Density(outputs['l2a1'],sig)     # emit "2"
 
 l2b1 = aug_leaf(0.95)
-l2b1.set_Obs_Density(6,sig)     # emit "4"
-l2b1.Name = 'L2b1'
+l2b1.Name = 'l2b1'
+l2b1.set_Obs_Density(outputs['l2b1'],sig)     # emit "4"
 
 l21 = b3.Sequence([l2a1,l2b1]) 
 
 # try 2
 l2a2 = aug_leaf(0.9)
-l2a2.set_Obs_Density(8,sig)     # emit "12"
-l2a2.Name = 'L2a2'
+l2a2.Name = 'l2a2'
+l2a2.set_Obs_Density(outputs['l2a2'],sig)     # emit "12"
 
 l2b2 = aug_leaf(0.95)
-l2b2.set_Obs_Density(10,sig)     # emit "12"
-l2b2.Name = 'L2b2'
+l2b2.Name = 'l2b2'
+l2b2.set_Obs_Density(outputs['l2b2'],sig)     # emit "12"
 
 l22 = b3.Sequence([l2a2,l2b2]) 
 
@@ -121,59 +134,61 @@ l2 = b3.Priority([l21,l22])
 ##########  Steps 3-5  Lift clear / reorient / move
 
 l345 = aug_leaf(1.0)
-l345.set_Obs_Density(12,sig)
-l345.Name = 'L345'
+l345.Name = 'l345'
+l345.set_Obs_Density(outputs['l345'],sig)
 
 ##########  Step 6 Insert Right grasper / grasp
 
 # try 1
 l6a1 = aug_leaf(0.6)
-l6a1.set_Obs_Density(14,sig)
-l6a1.Name = 'L6a1'
+l6a1.Name = 'l6a1'
+l6a1.set_Obs_Density(outputs['l6a1'],sig)
 
 l6b1 = aug_leaf(0.75)
-l6b1.set_Obs_Density(16,sig)
-l6b1.Name = 'L6b1'
+l6b1.Name = 'l6b1'
+l6b1.set_Obs_Density(outputs['l6b1'],sig)
 
 # try 2
 l6a2 = aug_leaf(0.6)
-l6a2.set_Obs_Density(18,sig)
-l6a2.Name = 'L6a2'
+l6a2.Name = 'l6a2'
+l6a2.set_Obs_Density(outputs['l6a2'],sig)
 
 l6b2 = aug_leaf(0.75)
-l6b2.set_Obs_Density(20,sig)
-l6b2.Name = 'L6b2'
+l6b2.Name = 'l6b2'
+l6b2.set_Obs_Density(outputs['l6b2'],sig)
 
 l61 = b3.Sequence([l6a1,l6b1])
 l62 = b3.Sequence([l6a2,l6b2])
 l6  = b3.Priority([l61,l62]) 
-l6.Name = "Leaf 6"
+l6.Name = "leaf 6"
 
 ########  Steps 7-9   Release Left / Reorient / Position
 
 l789 = aug_leaf(1.0)
-l789.set_Obs_Density(22,1)
-l789.Name = 'L789'
+l789.Name = 'l789'
+l789.set_Obs_Density(outputs['l789'],sig)
 
 ########  Step 10     Place on peg / Release / Clear 
      
 l10a1 = aug_leaf(0.9)
-l10a1.set_Obs_Density(24,1)
-l10a1.Name = 'L10a1'
+l10a1.Name = 'l10a1'
+l10a1.set_Obs_Density(outputs['l10a1'],sig)
 
 l10b1 = aug_leaf(0.95)
-l10b1.set_Obs_Density(26,1)
-l10b1.Name = 'L10c'
+l10b1.Name = 'l10b1'
+l10b1.set_Obs_Density(outputs['l10b1'],sig)
     
 l10c1 = aug_leaf(0.8)
-l10c1.set_Obs_Density(28,1)
-l10c1.Name = 'L10d'
+l10c1.Name = 'l10c1'
+l10c1.set_Obs_Density(outputs['l10c1'],sig)
 
 l10 = b3.Sequence([l10a1,l10b1,l10c1])
 l10.Name = 'Position/Release'
 
 ###    Debugging
 
+
+#quit()
 
 N1 = b3.Sequence([l1, l2, l345, l6, l789, l10])
 N1.Name = 'Sequencer Node'
