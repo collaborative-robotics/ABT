ed2b4a5b380a4f85e8d0a828e617dad38df1d9a7
Blake Hannaford <blake.hannaford@gmail.com>
2018-05-14 05:28:07 -0700

    separate out abtclass

--- peg2.py
+++ peg2.py
@@ -5,13 +5,14 @@
 #     from BT-Hmm proposal   May 18
 
 
-
-# modified by BH, local version in current dir
 import os as os
+
+# b3 class modified by BH, local version in current dir
 import b3 as b3          # behavior trees
 import random as random
 import math as m
 import numpy as np
+from abtclass import *
  
 # BT and HMM parameters here
 from  model01 import *
@@ -29,68 +30,6 @@ SolverDebug = False
 if not os.path.isdir(logdir):  # if this doesn't exist, create it.
     os.mkdir(logdir) 
     
-
-def gaussian(x, mu, sig):
-    a = 1.0/(sig*(m.sqrt(2*m.pi))) * m.exp(-0.5*((x-mu)/sig)**2)
-    #print "A gaussian: ", a
-    return a
-
-class aug_leaf(b3.Action): 
-    def __init__(self,probSuccess):
-        b3.BaseNode.__init__(self)
-        # Transition Probabilities for this leaf
-        self.pS = probSuccess
-        self.pF = 1.0-self.pS
-        #  Observation Densities for this leaf
-        self.Obs = np.zeros(NSYMBOLS)
-        # give a residual obs prob:
-        for j in range(NSYMBOLS):
-            self.Obs[j] = 0.0001  # a nominal non-zero value
-        
-    def set_Obs_Density(self, mu, sig):
-        if (mu+sig) > NSYMBOLS or ((mu-sig) < 0):
-            print 'aug_leaf: Warning may gen negative observations'
-            #quit()
-        psum = 0.0
-        for j in range(NSYMBOLS):
-            self.Obs[j] += gaussian(j+0.5,mu,sig)
-            psum += self.Obs[j]
-            #print "j/Obs:",j,self.Obs[j]
-        #normalize the Observation density so it sums to 1.000
-        for j in range(NSYMBOLS):
-            self.Obs[j] /= psum
-            
-        #print self.Name, 'obs:', mu, sig
-        
-        
-    # initialize Success Prob for leaf    
-    def set_Ps(self, P):
-        self.pS = P
-        self.pF = 1.0-P
-        
-    def gen_obs(self):
-        a = random.uniform(0,1.0)
-        b = 0.0
-        for j in range(NSYMBOLS):
-            b += self.Obs[j]
-            #print "Obs: b,a", b,a
-            if b >= a:
-                return j;
-        return j
-    
-    def tick(self,tick):
-        f = tick.blackboard.get('logfileptr')
-        a = random.uniform(0,1.0)
-        #print "random: ",a , "   Ps: ", self.pS
-        observation = self.gen_obs()
-        #print "------------------------------------> obs: ", observation
-        f.write(self.Name+', '+str(observation)+'\n')
-        if a<self.pS:
-            return b3.SUCCESS 
-        else:
-            return b3.FAILURE 
-print"\n\n"
-
 
 #print outputs
 #quit()
@@ -209,8 +148,6 @@ for l in leafs:
     print ''
 
 ###    Debugging
-
-
 #quit()
 # open the log file
 logf = open(logdir+'statelog.txt','w')
@@ -219,7 +156,7 @@ bb.set('logfileptr',logf)
 demo_bt.root = N1
 
 for i in range(NEpochs):
-    demo_bt.tick("First test tree for ABTs", bb)
+    demo_bt.tick("ABT Simulation", bb)
     logf.write('---\n')
     
 logf.close()
