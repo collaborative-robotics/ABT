450959d0479d5c29493d801df3943c777f06b5cd
blake <blake.hannaford@gmail.com>
2018-09-05 09:42:23 -0700

    working on big test

--- tl_bw_hmm_big.py
+++ tl_bw_hmm_big.py
@@ -19,6 +19,7 @@ from abt_constants import *
 ##   Set up research parameters
 
 CSVOUTPUT = True
+METAOUTPUT = True
 
 ############################################
 #
@@ -29,10 +30,14 @@ NEWDATA = True  # flag to generate data once
 
 task = BaumWelch   # Viterbi / Forward
 
-assert len(sys.argv) == 2, 'Need a command line argument (HMM_delta)'
 
+#assert len(sys.argv) == 2, 'Need a command line argument (HMM_delta)'
+#script_name = 'bw_hmm_big'+ str(sys.argv[1])
 
-script_name = 'bw_hmm_big'+ str(sys.argv[1])
+script_name = 'bw_hmm_big_meta'
+
+if METAOUTPUT:
+    fmeta = open('meta_output.csv','w')
 
 
 # amount HMM parameters should be ofset
@@ -69,7 +74,7 @@ if CSVOUTPUT:
     fcsv = open('csvlog'+script_name,'a')
     print >> fcsv, '-------',datetime.datetime.now().strftime("%y-%m-%d-%H-%M"), 'Nruns: ', Nruns, 'x', NEpochs, ' #states: ',len(names)
     #task, Ratio, int(di), float(di)/float(sig),run+1,Nruns,e2,em)
-    print >> fcsv, 'N  tsk Ratio     di   Sigma  run#       e2  emax '
+    print >> fcsv, 'tsk Hdelt Ratio   di   Sigma  run#  e2  emax '
 
 nsims = 0
 e2T = 0.0
@@ -80,144 +85,156 @@ bigratiotest = [0.25, 0.5, 1, 5.0, 0.25, 0.5, 1, 5.0, 0.25, 0.5, 1, 5.0]
 
 smallratiotest = [0.25, 5.0]
 
-HMM_delta = float(sys.argv[1])
+#####    make a string report describing the setup
+#
+#
+rep = []
+rep.append('-------------------------- BT to HMM ---------------------------------------------')
+rep.append(datetime.datetime.now().strftime("%y-%m-%d-%H:%M"))
+rep.append('NSYMBOLS: {:d}   NEpochs: {:d} N-States: {:d} '.format(NSYMBOLS,NEpochs,len(names)))
+rep.append('sigma: {:.2f}    Symbol delta: {:d}   Ratio:  {:.2f}'.format(sig, int(di), float(di)/float(sig)))
+rep.append('----------------------------------------------------------------------------------')
+rep.append(' ')
+
+if METAOUTPUT:
+    print >>fmeta, rep
 
 #################################################
 #
 #   Outer Loop
 for Ratio in smallratiotest:
-    ########################
-    # 
-    #  run loop 
+    print 'Starting run with Ratio = ',Ratio
+    ################################################
     #
-    for run in range(Nruns):
-
-        print '\n-------------------------------------------\n   Starting Run ',run+1, 'of', Nruns, '\n\n'
-        # open the log file
-        id = str(int(100*(Ratio)))+'iter'+str(run)  # encode the ratio (delta mu/sigma) into filename
-        lfname = logdir+script_name+'_statelog.txt'
-
-
-        #####    make a string report describing the setup
+    #   amount initial HMM params are changed from exact
+    #
+    for HMM_delta in [0.5, 1.0, 5.0]:
+        print 'Starting run with Hmm_delta = ', HMM_delta
+        ########################
+        # 
+        #  run loop 
         #
-        #
-        rep = []
-        rep.append('-------------------------- BT to HMM ---------------------------------------------')
-        rep.append('NSYMBOLS: {:d}   NEpochs: {:d} N-States: {:d} '.format(NSYMBOLS,NEpochs,len(names)))
-        rep.append('sigma: {:.2f}    Symbol delta: {:d}   Ratio:  {:.2f}'.format(sig, int(di), float(di)/float(sig)))
-        rep.append('----------------------------------------------------------------------------------')
-        rep.append(' ')
+        for run in range(Nruns):
+
+            print '\n-------------------------------------------\n   Starting Run ',run+1, 'of', Nruns, '\n\n'
+            # open the log file
+            id = str(int(100*(Ratio)))+'iter'+str(run)  # encode the ratio (delta mu/sigma) into filename
+            lfname = logdir+script_name+'_statelog.txt'
 
 
-        #############################################
-        #
-        #    Set up models
-
-
-        #############################################
-        #
-        #    Build the ABT and its blackboard
-        #
-
-        [ABT, bb] = ABTtree()  # defined in xxxxxxABT.py file
-
-        #############################################
-        #
-        #    Generate Simulated Data only on first round
-        #
-        if(NEWDATA):
-            seq_data_f = open(lfname,'w')
-            bb.set('logfileptr',seq_data_f)   #allow BT nodes to access file
-            osu = names[-2]  # state names
-            ofa = names[-1]
-
-            for i in range(NEpochs):
-                result = ABT.tick("ABT Simulation", bb)
-                if (result == b3.SUCCESS):
-                    seq_data_f.write('{:s}, {:.0f}\n'.format(osu,outputs[osu]))  # not random obs!
-                else:
-                    seq_data_f.write('{:s}, {:.0f}\n'.format(ofa,outputs[ofa]))
-                seq_data_f.write('---\n')
-
-            seq_data_f.close()
-            seq_data_f = open(lfname,'r') # save file and reset pointer
-
-            print 'Finished simulating ',NEpochs,'  epochs'
-
-        NEWDATA = False
-        #############################################
-        #
-        #    Read simulated sequence data
-        #
-
-        seq_data_f = open(lfname,'r')
-        [X,Y,Ls] = read_obs_seqs(seq_data_f)
-        seq_data_f.close()
-
-        assert len(Y) > 0, 'Empty observation sequence data'
-
-        # remove the old log file
-        #os.system('rm '+lfname)
-
-        #############################################
-        #
-        #    HMM setup
-        #
-        Ac = A.copy()
-        M = HMM_setup(Pi,Ac,sig,names)
-
-        #############################################
-        #
-        #   Perturb the HMM's parameters (optional)
-        #
-        #outputAmat(M.transmat_,'Model A matrix',names,sys.stdout)
-
-        A_row_test(M.transmat_, sys.stdout)
-
-        if(HMM_delta > 0.0001):
-            HMM_perturb( M, HMM_delta )
-
-        A_row_test(M.transmat_, sys.stdout)
-
-        if(task == BaumWelch):
             #############################################
             #
-            #   Identify HMM params with Baum-Welch
+            #    Set up models
+
+
+            #############################################
             #
-            print "starting HMM fit with ", len(Y), ' observations.'
+            #    Build the ABT and its blackboard
+            #
 
-            M.fit(Y,Ls)
-            # print the output file header
-            for rline in rep:
-                print >>of, rline
+            [ABT, bb] = ABTtree()  # defined in xxxxxxABT.py file
 
-            outputAmat(A,"Original A Matrix", names, of)
-            outputAmat(M.transmat_,"New A Matrix", names, of)
+            #############################################
+            #
+            #    Generate Simulated Data only on first round
+            #
+            if(NEWDATA):
+                seq_data_f = open(lfname,'w')
+                bb.set('logfileptr',seq_data_f)   #allow BT nodes to access file
+                osu = names[-2]  # state names
+                ofa = names[-1]
+
+                for i in range(NEpochs):
+                    result = ABT.tick("ABT Simulation", bb)
+                    if (result == b3.SUCCESS):
+                        seq_data_f.write('{:s}, {:.0f}\n'.format(osu,outputs[osu]))  # not random obs!
+                    else:
+                        seq_data_f.write('{:s}, {:.0f}\n'.format(ofa,outputs[ofa]))
+                    seq_data_f.write('---\n')
+
+                seq_data_f.close()
+
+                print 'Finished simulating ',NEpochs,'  epochs'
+
+            NEWDATA = False
+            #############################################
+            #
+            #    Read simulated sequence data
+            #
+            X = []
+            Y = []
+            Ls = []
+            seq_data_f = open(lfname,'r')
+            [X,Y,Ls] = read_obs_seqs(seq_data_f)
+            seq_data_f.close()
+
+            assert len(Y) > 0, 'Empty observation sequence data'
+
+            # remove the old log file
+            #os.system('rm '+lfname)
+
+            #############################################
+            #
+            #    HMM setup
+            #
+            Ac = A.copy()
+            M = HMM_setup(Pi,Ac,sig,names)
+
+            #############################################
+            #
+            #   Perturb the HMM's parameters (optional)
+            #
+            #outputAmat(M.transmat_,'Model A matrix',names,sys.stdout)
+
+            A_row_test(M.transmat_, sys.stdout)
+
+            if(HMM_delta > 0.0001):
+                HMM_perturb( M, HMM_delta )
+
+            A_row_test(M.transmat_, sys.stdout)
+
+            if(task == BaumWelch):
+                #############################################
+                #
+                #   Identify HMM params with Baum-Welch
+                #
+                print "starting HMM fit with ", len(Y), ' observations.'
+
+                M.fit(Y,Ls)
+                # print the output file header
+                for rline in rep:
+                    print >>of, rline
+
+                outputAmat(A,"Original A Matrix", names, of)
+                outputAmat(M.transmat_,"New A Matrix", names, of)
 
 
-            ##  compare the two A matrices
-            #     (compute error metrics)
-            [e,e2,em,N2,im,jm,anoms,erasures] = Adiff(A,M.transmat_, names)
+                ##  compare the two A matrices
+                #     (compute error metrics)
+                [e,e2,em,N2,im,jm,anoms,erasures] = Adiff(A,M.transmat_, names)
 
-            print >> of, 'EAavg    A-matrix error: {:.8f} ({:d} non zero elements)'.format(e2,N2)
-            print >> of, 'EAinfty  A-matrix error: {:.3f} (at {:d} to {:d})'.format(em,im,jm)
-            if len(anoms) == 0:
-                anoms = 'None'
-            print >> of, 'Anomalies: ', anoms
-            if len(erasures) == 0:
-                anoms = 'None'
-            print >> of, 'Erasures : ', erasures
+                print >> of, 'EAavg    A-matrix error: {:.8f} ({:d} non zero elements)'.format(e2,N2)
+                print >> of, 'EAinfty  A-matrix error: {:.3f} (at {:d} to {:d})'.format(em,im,jm)
+                if len(anoms) == 0:
+                    anoms = 'None'
+                print >> of, 'Anomalies: ', anoms
+                if len(erasures) == 0:
+                    anoms = 'None'
+                print >> of, 'Erasures : ', erasures
 
-        if CSVOUTPUT:
-            print >>fcsv, '{:2d}, {:.3f}, {:3d}, {:.3f}, {:2d}, {:2d}, {:.3f}, {:.3f}'.format(task, Ratio, int(di), float(sig),run+1,Nruns,e2,em)
+            if CSVOUTPUT:
+                print >>fcsv, '{:2d}, {:.3f}, {:3d}, {:.3f}, {:2d}, {:2d}, {:.3f}, {:.3f}'.format(task, Ratio, int(di), float(sig),run+1,Nruns,e2,em)
 
-        nsims += 1
-        emT += em
-        e2T += e2
-        # update an information log on this run
-        print >> infolog, datetime.datetime.now().strftime("%y-%m-%d-%H-%M"), 'task: ', task, ' run ',run+1,'/',Nruns, ' NEpochs: ', NEpochs,'Emax: ', em
-        infolog.flush()    # make sure this info visible in file
-        os.fsync(infolog.fileno())
+            if METAOUTPUT:
+                print >>fmeta, '{:2d}, {:.3f}, {:.3f}, {:3d}, {:.3f}, {:2d}, {:2d}, {:.3f}, {:.3f}'.format(task, Ratio, HMM_delta, int(di), float(sig),run+1,Nruns,e2,em)
+
+            nsims += 1
+            emT += em
+            e2T += e2
+            # update an information log on this run
+            print >> infolog, datetime.datetime.now().strftime("%y-%m-%d-%H-%M"), 'task: ', task, ' run ',run+1,'/',Nruns, ' NEpochs: ', NEpochs,'Emax: ', em
+            infolog.flush()    # make sure this info visible in file
+            os.fsync(infolog.fileno())
 
     #  End of loop of runs
 
@@ -225,6 +242,7 @@ for Ratio in smallratiotest:
         print >>fcsv, '{:3d} {:s} {:.3f}, {:.3f}'.format(task, 'Average e2, em: ',e2T/nsims,emT/nsims)
         
         
+fmeta.close()
 fcsv.close()
 of.close()
 os.system('cp {:s} {:s}'.format(oname,outputdir+'lastoutput'))
