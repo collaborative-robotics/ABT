67543ec12e773af514de22c3084658f3892d3d5c
Blake Hannaford <blake.hannaford@gmail.com>
2018-09-13 09:32:17 -0700

    perfecting test version of tl_

--- tl_bw_test.py
+++ tl_bw_test.py
@@ -10,6 +10,7 @@
 
 import sys
 import os
+import subprocess
 import uuid
 import datetime
 from hmm_bt import *
@@ -19,11 +20,26 @@ from abt_constants import *
 MODEL = BIG
 
 ##
-#     Uncomment to supress Deprecation Warnings from hmm_lean / scikit
+#    Supress Deprecation Warnings from hmm_lean / scikit
 import warnings
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 ##   Set up research parameters mostly in abt_constants.py
+
+############################################
+
+##  The ABT file for the task (CHOOSE ONE)
+
+if MODEL== BIG:
+    from peg2_ABT import * # big  14+2 state  # uses model01.py
+    from model01 import *
+    model = modelo01
+    
+if MODEL==SMALL:
+    from simp_ABT import *  # small 4+2 state # uses model02.py
+    from model00 import *
+    model = modelo00
+
 
  
 ############################################
@@ -34,21 +50,39 @@ warnings.filterwarnings('ignore', category=DeprecationWarning)
 NEWDATA = True  # flag to generate data once
 
 task = BaumWelch   # Viterbi / Forward
- 
 
+script_name = 'bw_hmm'
+
+
+# amount HMM parameters should be ofset
+#   from the ABT parameters.  Offset has random sign (+/-)
+
+if len(sys.argv) != 1:
+    print 'TEST VERSION:'
+    print 'Edit your params on line 53.\n  You entered:'
+    print sys.argv
+    quit()
+    
+comment = 'xxxxxx TESTING xxxxx'
+
+Nruns = 3  #testing
+sig = 2.000
+Ratio = 3.00  #testing
+HMM_delta = 0.244  #testing
+NEpochs = 5000    # testing
+
+#################################################
+#     Normally 0.0 < HMM_delta < 0.500
+###   As a flag, if HMM_delta > 5.0 it is a signal 
+#        that HMM initial A matrix should be set to RANDOM
+HMM_RANDOM_INIT = False
+if HMM_delta > 4.95:
+    HMM_RANDOM_INIT = True
+
+
+print 'Nruns v1: ',Nruns
 #
-#    Change non-zero A-matrix elements to RANDOM [0.0-1.0)
-#
-
-#
-############################################
-
-##  The ABT file for the task (CHOOSE ONE)
-
-if MODEL== BIG:
-    from peg2_ABT import * # big  14+2 state  # uses model01.py
-if MODEL==SMALL:
-    from simp_ABT import *  # small 4+2 state # uses model02.py
+print 'Nruns v2: ',Nruns
 
 #############################################
 #
@@ -62,40 +96,44 @@ if MODEL==SMALL:
 #
 #
 
-comment = 'testing new output system: 0.5Ratio'
-
 ownname = sys.argv[0]
  
-datadir = 'bw_output/'
-seqdir  = 'sequences/'
+git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])[:10]  # first 10 chars to ID software version
+
+datadir = 'test/'
+seqdir  = 'test/'
+
+urunid = str(uuid.uuid4())  # a unique hash code for this run 
 
 #if these don't exist, create them 
 for ndir in [datadir, seqdir]:
     if not (os.path.exists(os.path.dirname(ndir))):
         os.mkdir(ndir)
 
+
 metadata_name = 'hmm_bw_metadata.txt'
 # Metadata file format:  each line: (comma sep)
 #
-# 1) date and time stamp
-# 2) name of data file
-# 3) ownname  (name of the top level file)
-# 4) number of HMM / BT states
-# 5) text field (comment)
+#  0) date and time stamp
+#  1) name of data file
+#  2) ownname  (name of the top level file)
+#  3) git hash (1st 10 chars of current git hash)
+#  4) number of HMM / BT states
+#  5) text field (comment)
 #
-datafile_name = datadir+'data_'+str(uuid.uuid4())+'.csv'  # a unique filename
+datafile_name = datadir+'data_'+urunid+'.csv'  # a unique filename
 # Datafile format:  comma sep
 #
-#  1)  Task code (2=Baum Welch)
-#  2)  Ratio  (codeword mean spacing / sigma)
-#  3)  di     (codeword spacing)
+#  0)  Task code (2=Baum Welch)
+#  1)  Ratio  (codeword mean spacing / sigma)
+#  2)  di     (codeword spacing)
+#  3)  HMM_delta    amt HMM params changed
 #  4)  Sigma
 #  5)  run#
-#  6)  # of runs
-#  7)  e2 (RMS error)
-#  8)  emax (max error)
+#  6)  e2 (RMS error)
+#  7)  emax (max error)
 
-sequence_name =  seqdir+'seq_'+str(uuid.uuid4())
+sequence_name =  seqdir+'seq_'+urunid+'.txt'   # name of sim sequence file
 #
 #  sequence file format
 #
@@ -105,24 +143,19 @@ sequence_name =  seqdir+'seq_'+str(uuid.uuid4())
 
 fmeta = open(metadata_name, 'a')  #  append metadata to a big log
 fdata = open(datafile_name, 'w')  #  unique filename for csv output 
-# open sequence_name   in NEWDATA section below 
-
-em = 9999
+# open sequence_name   in NEWDATA section below  
 
 nsims = 0
 e2T = 0.0
 emT = 0.0
 
-Nruns = 10  #testing
-sig = 2.000
-Ratio = 0.50  #testing
-HMM_delta = 0.2  #testing
-NEpochs = 20000    # testing
+print 'Nruns v3: ',Nruns
 
-di = int(Ratio*sig)   # change in output obs mean per state
+print '-----'
+print 'Model Size: ', model.n
 
 ##  output the metadata
-line = '{:s} | {:s} | {:s} | {:d} | {:s}'.format(datetime.datetime.now().strftime("%y-%m-%d-%H:%M"), datafile_name, ownname, len(names),  comment)
+line = '{:s} | {:s} | {:s} | {:s} | {:d} | {:s}'.format(datetime.datetime.now().strftime("%y-%m-%d-%H:%M"), datafile_name, ownname, git_hash, model.n,  comment)
 print >> fmeta , line
 
 #################################################
@@ -132,8 +165,6 @@ print >> fmeta , line
 for run in range(Nruns):
 
     print '\n-------------------------------------------\n   Starting Run ',run+1, 'of', Nruns, '\n\n'
-    # open the log file
-    id = str(int(100*(Ratio)))+'iter'+str(run)  # encode the ratio (delta mu/sigma) into filename
  
     #####    make a string report describing the setup
     #
@@ -142,7 +173,7 @@ for run in range(Nruns):
     rep.append('-------------------------- BT to HMM ---------------------------------------------')
     stringtime = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
     rep.append(stringtime)
-    rep.append('NSYMBOLS: {:d}   NEpochs: {:d} N-States: {:d} '.format(NSYMBOLS,NEpochs,len(names)))
+    rep.append('NSYMBOLS: {:d}   NEpochs: {:d} N-States: {:d} '.format(NSYMBOLS,NEpochs,len(model.names)))
     rep.append('HMM_delta: {:.3f}'.format(HMM_delta))
     rep.append('sigma: {:.2f}    Symbol delta: {:d}   Ratio:  {:.2f}'.format(sig, int(di), float(di)/float(sig)))
     rep.append('----------------------------------------------------------------------------------')
@@ -159,7 +190,7 @@ for run in range(Nruns):
     #    Build the ABT and its blackboard
     #
 
-    [ABT, bb] = ABTtree()  # defined in xxxxxxABT.py file
+    [ABT, bb] = ABTtree(model)  # defined in xxxxxxABT.py file
 
     #############################################
     #
@@ -168,15 +199,15 @@ for run in range(Nruns):
     if(NEWDATA):
         seq_data_f = open(sequence_name,'w')
         bb.set('logfileptr',seq_data_f)   #allow BT nodes to access file
-        osu = names[-2]  # state names
-        ofa = names[-1]
+        osu = model.names[-2]  # state names
+        ofa = model.names[-1]
 
         for i in range(NEpochs):
             result = ABT.tick("ABT Simulation", bb)
             if (result == b3.SUCCESS):
-                seq_data_f.write('{:s}, {:.0f}\n'.format(osu,outputs[osu]))  # not random obs!
+                seq_data_f.write('{:s}, {:.0f}\n'.format(osu,model.outputs[osu]))  # not random obs!
             else:
-                seq_data_f.write('{:s}, {:.0f}\n'.format(ofa,outputs[ofa]))
+                seq_data_f.write('{:s}, {:.0f}\n'.format(ofa,model.outputs[ofa]))
             seq_data_f.write('---\n')
 
         seq_data_f.close()
@@ -189,65 +220,81 @@ for run in range(Nruns):
     #    Read simulated sequence data
     #
 
-    seq_data_f = open(sequence_name,'r')
-    X = []
     Y = []
+    X = []
     Ls = []
+    seq_data_f = open(sequence_name,'r')
     [X,Y,Ls] = read_obs_seqs(seq_data_f)
     seq_data_f.close()
 
     assert len(Y) > 0, 'Empty observation sequence data'
 
-    # remove the old log file
-    #os.system('rm '+lfname)
 
     #############################################
     #
     #    HMM setup
     #
-    Ac = A.copy()
-    M = HMM_setup(Pi,Ac,sig,names)
-
-
+    Ac = A.copy()  # isolate orig A matrix from HMM
+    Ar = A.copy()  # reference original copy
+    M = HMM_setup(Pi,Ac,sig,model.names)
 
     #############################################
     #
     #   Perturb the HMM's parameters (optional)
     #
-    #outputAmat(M.transmat_,'Model A matrix',names,sys.stdout)
+    #outputAmat(M.transmat_,'Model A matrix',model.names,sys.stdout)
 
-    A_row_test(M.transmat_, sys.stdout)
+    A_row_test(M.transmat_, sys.stdout)   # Make sure A-Matrix Valid
 
-    #HMM_ABT_to_random(M)   # randomize probabilites
-    #print 'Applied Random Matrix Perturbation'
-    HMM_perturb(M, HMM_delta)
-    print 'Applied Matrix Perturbation: ' + str(HMM_delta)
-    A_row_test(M.transmat_, sys.stdout)
-    B = M.transmat_.copy()  # store perturbed
+    
+    testeps = 0.00001
+    if(not HMM_RANDOM_INIT and HMM_delta > testeps):
+        #HMM_ABT_to_random(M)   # randomize probabilites
+        #print 'Applied Random Matrix Perturbation'
+        HMM_perturb(M, HMM_delta)
+        print 'Applied Matrix Perturbation: ' + str(HMM_delta)
+        
 
+    if (HMM_RANDOM_INIT):
+        A_rand = A.copy() 
+        [rn,cn] = A_rand.shape
+        for r in range(rn):      # normalize the rows
+            rsum = 0.0
+            for c in range(cn):
+                A_rand[r][c] = random.random()
+                rsum += A_rand[r][c]
+            for c in range(cn):
+                A_rand[r][c] /= rsum
+        M.transmat_ = A_rand
+        print 'Applied FULLY RANDOM Matrix Perturbation: '
+        outputAmat(M.transmat_, 'RANDOM a-mat', model.names)
+    
+    
+    A_row_test(M.transmat_, sys.stdout)   # Make sure A-Matrix Valid
 
     # special test code
     #  compare the two A matrices
     #     (compute error metrics)
     testeps = 0.00001
-    [e,e2,em,N2,im,jm,anoms,erasures] = Adiff(A,M.transmat_, names)
+    if HMM_delta > testeps: 
+        [e,e2,em,N2,im,jm,anoms,erasures] = Adiff(Ar,M.transmat_, model.names)
 
-    ##  some assertions to make sure pertubations are being done right
-    #   (if they aren't there's not point in doing the sim)
-    assert em > 0.0 , 'Perturbation caused no difference in A matrices'
-    assert e2 > 0.0 , 'Perturbation caused no difference in A matrices'
-    print 'Model Size: ',len(names)
-    if len(names) < 8:
-        outS_index = 4
-    else:
-        outS_index = 14
-    outF_index = outS_index+1
-    assert M.transmat_[outS_index,outS_index] - 1.0 < testeps, 'A 1.0 element was modified'
-    assert M.transmat_[outF_index,outF_index] - 1.0 < testeps, 'A 1.0 element was modified'
+        
+        ##  some assertions to make sure pertubations are being done right
+        #   (if they aren't there's not point in doing the sim)
+        assert em > 0.0 , 'Perturbation caused no difference in A matrices'
+        assert e2 > 0.0 , 'Perturbation caused no difference in A matrices'
+        print 'em: {:.2f}'.format(em)
+        print 'e2: {:.2f}'.format(e2)
+        if model.n < 8:
+            outS_index = 4
+        else:
+            outS_index = 14
+        outF_index = outS_index+1
+        assert M.transmat_[outS_index,outS_index] - 1.0 < testeps, 'A 1.0 element was modified'
+        assert M.transmat_[outF_index,outF_index] - 1.0 < testeps, 'A 1.0 element was modified'
     print 'Passed A-matrix Assertions'
     #end of special test code
-
-
 
     if(task == BaumWelch):
         #############################################
@@ -257,18 +304,18 @@ for run in range(Nruns):
         print "starting HMM fit with ", len(Y), ' observations.'
 
         M.fit(Y,Ls)
-        ## print the output file header
+        # print the output file header
         #for rline in rep:
             #print >>of, rline
 
-        #outputAmat(A,"Original A Matrix", names, of)
-        #outputAmat(B,"Perturbed A Matrix", names, of)
-        #outputAmat(M.transmat_,"New A Matrix (pertb + HMM fit)", names, of)
+        #outputAmat(A,"Original A Matrix", model.names, of)
+        #outputAmat(B,"Perturbed A Matrix", model.names, of)
+        #outputAmat(M.transmat_,"New A Matrix (pertb + HMM fit)", model.names, of)
 
 
         ##  compare the two A matrices
         #     (compute error metrics)
-        [e,e2,em,N2,im,jm,anoms,erasures] = Adiff(A,M.transmat_, names)
+        [e,e2,em,N2,im,jm,anoms,erasures] = Adiff(A,M.transmat_, model.names)
 
         #print >> of, 'EAavg    A-matrix error: {:.8f} ({:d} non zero elements)'.format(e2,N2)
         #print >> of, 'EAinfty  A-matrix error: {:.3f} (at {:d} to {:d})'.format(em,im,jm)
@@ -280,7 +327,7 @@ for run in range(Nruns):
             anoms = 'None'
         #print >> of, 'Erasures : ', erasures
 
-        print >>fdata, '{:2d}, {:.3f}, {:3d}, {:.3f}, {:2d}, {:2d}, {:.3f}, {:.3f}'.format(task, Ratio, int(di), float(sig),run+1,Nruns,e2,em)
+        print >>fdata, '{:2d}, {:.3f}, {:3d}, {:.3f}, {:.3f}, {:2d}, {:.3f}, {:.3f}'.format(task, Ratio, int(di), HMM_delta, float(sig), run+1, e2,em)
 
     nsims += 1
     emT += em
